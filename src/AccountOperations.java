f77c3005a1a099ee2cf75dec995a8c3493697e6d
Kacper Kisielewski <s30670@pjwstk.edu.pl>
2024-06-12 10:29:38 +0200

    reformatting

--- src/AccountOperations.java
+++ src/AccountOperations.java
@@ -1,5 +1,7 @@
 public interface AccountOperations {
     double checkBalance();
+
     void makeTransfer(AccountOperations receiverAccount, double amount) throws AccountOperationException;
+
     void makeDeposit(double amount);
 }
